8f1af5023b06c2706a4c71a6dffc27857bd6c229
Jeremy Ward <jeremy@jmichaelward.com>
2021-07-31 19:16:10 -0500

    Set up two basic route handlers.

--- go-web-app.go
+++ go-web-app.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "All about me!")
+}
+
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/about", AboutHandler)
+
+	http.Handle("/", router)
+
+	http.ListenAndServe(":80", router)
 }
